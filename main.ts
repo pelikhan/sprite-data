1459cacd25328eca4155ab19b05dcf7adba07af8
Peli de Halleux <pelikhan@users.noreply.github.com>
2020-04-02 19:58:00 -0700

    add block annotations

--- main.ts
+++ main.ts
@@ -2,9 +2,10 @@ namespace sprites {
     /**
      * Sets a number in the data of a sprite
      */
-    //% blockId=spriteDataSetNumber block="set $sprite data $name to number $value"
+    //% blockId=spriteDataSetNumber block="set $sprite=variables_get data $name to number $value"
     //% group="Data"
     //% weight=10
+    //% blockGap=8
     export function setDataNumber(sprite: Sprite, name: string, value: number) {
         if (!sprite) return;
         const d = sprite.data();
@@ -14,9 +15,10 @@ namespace sprites {
     /**
      * Gets a number in the data of a sprite
      */
-    //% blockId=spriteDataGetNumber block="$sprite data $name as number"
+    //% blockId=spriteDataGetNumber block="$sprite=variables_get data $name as number"
     //% group="Data"
     //% weight=10
+    //% blockGap=8
     export function readDataNumber(sprite: Sprite, name: string): number {
         if (!sprite) return 0;
         const d = sprite.data();
@@ -26,9 +28,10 @@ namespace sprites {
     /**
      * Sets a string in the data of a sprite
      */
-    //% blockId=spriteDataSetString block="set $sprite data $name to string $value"
+    //% blockId=spriteDataSetString block="set $sprite=variables_get data $name to string $value"
     //% group="Data"
     //% weight=10
+    //% blockGap=8
     export function setDataString(sprite: Sprite, name: string, value: string) {
         if (!sprite) return;
         const d = sprite.data();
@@ -38,9 +41,10 @@ namespace sprites {
     /**
      * Gets a number in the data of a sprite
      */
-    //% blockId=spriteDataGetNumber block="$sprite data $name as number"
+    //% blockId=spriteDataGetNumber block="$sprite=variables_get data $name as number"
     //% group="Data"
     //% weight=10
+    //% blockGap=8
     export function readDataString(sprite: Sprite, name: string): string {
         if (!sprite) return "";
         const d = sprite.data();
